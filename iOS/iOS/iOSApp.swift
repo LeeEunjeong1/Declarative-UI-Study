20366a4fa289cf29ef029377d63d7b507f097b91
Jihyun <75922558+jkim68888@users.noreply.github.com>
2023-07-11 14:51:35 +0900

    ios project init

---
+++ iOS/iOS/iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp.swift
+//  iOS
+//
+//  Created by Financial CB on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
