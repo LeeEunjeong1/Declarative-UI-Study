9110f1712ebb8ff4befdaf0306cd2ae2171fab97
Jihyun <75922558+jkim68888@users.noreply.github.com>
2023-07-24 18:47:18 +0900

    상단 탭바 리팩토링 중

--- iOS/iOS/CustomTopTabBar.swift
+++ iOS/iOS/CustomTopTabBar.swift
@@ -8,27 +8,34 @@
 import SwiftUI
 
 struct CustomTopTabBar: View {
+    
+    private var tabBarItems: [String] = ["서머 바캉스", "추천", "랭킹", "스타일", "세일", "뷰티", "이벤트"]
 	@Binding var tabIndex: Int
 	
     var body: some View {
-        HStack {
-			TabBarButton(text: "서머 바캉스", isSelected: .constant(tabIndex == 0))
-				.onTapGesture { onButtonTapped(index: 0) }
-			TabBarButton(text: "추천", isSelected: .constant(tabIndex == 1))
-				.onTapGesture { onButtonTapped(index: 1) }
-			TabBarButton(text: "랭킹", isSelected: .constant(tabIndex == 2))
-				.onTapGesture { onButtonTapped(index: 2) }
-			TabBarButton(text: "스타일", isSelected: .constant(tabIndex == 3))
-				.onTapGesture { onButtonTapped(index: 3) }
-			TabBarButton(text: "세일", isSelected: .constant(tabIndex == 4))
-				.onTapGesture { onButtonTapped(index: 4) }
-			Spacer()
+//        ScrollView(.horizontal) {
+//			TabBarButton(text: "서머 바캉스", isSelected: .constant(tabIndex == 0))
+//				.onTapGesture { onButtonTapped(index: 0) }
+//			TabBarButton(text: "추천", isSelected: .constant(tabIndex == 1))
+//				.onTapGesture { onButtonTapped(index: 1) }
+//			TabBarButton(text: "랭킹", isSelected: .constant(tabIndex == 2))
+//				.onTapGesture { onButtonTapped(index: 2) }
+//			TabBarButton(text: "스타일", isSelected: .constant(tabIndex == 3))
+//				.onTapGesture { onButtonTapped(index: 3) }
+//			TabBarButton(text: "세일", isSelected: .constant(tabIndex == 4))
+//				.onTapGesture { onButtonTapped(index: 4) }
+//			Spacer()
+//        }
+        
+        TabView {
+            ForEach(tabBarItems.indices, id: \.self) { index in
+                Text(tabBarItems[index])
+                    .onTapGesture {
+                        <#code#>
+                    }
+            }
         }
     }
-	
-	private func onButtonTapped(index: Int) {
-		withAnimation { tabIndex = index }
-	}
 }
 
 //struct CustomTopTabBar_Previews: PreviewProvider {
