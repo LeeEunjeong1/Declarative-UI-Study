6dba636bf346fa560ce5d0cc3c2ac595a1df9fa2
Jihyun <75922558+jkim68888@users.noreply.github.com>
2023-07-27 14:15:57 +0900

    탭바랑 버튼 파일 합침

--- iOS/iOS/CustomTopTabBar.swift
+++ iOS/iOS/CustomTopTabBar.swift
@@ -9,32 +9,44 @@ import SwiftUI
 
 struct CustomTopTabBar: View {
     
-    private var tabBarItems: [String] = ["서머 바캉스", "추천", "랭킹", "스타일", "세일", "뷰티", "이벤트"]
 	@Binding var tabIndex: Int
-	
+    
     var body: some View {
-//        ScrollView(.horizontal) {
-//			TabBarButton(text: "서머 바캉스", isSelected: .constant(tabIndex == 0))
-//				.onTapGesture { onButtonTapped(index: 0) }
-//			TabBarButton(text: "추천", isSelected: .constant(tabIndex == 1))
-//				.onTapGesture { onButtonTapped(index: 1) }
-//			TabBarButton(text: "랭킹", isSelected: .constant(tabIndex == 2))
-//				.onTapGesture { onButtonTapped(index: 2) }
-//			TabBarButton(text: "스타일", isSelected: .constant(tabIndex == 3))
-//				.onTapGesture { onButtonTapped(index: 3) }
-//			TabBarButton(text: "세일", isSelected: .constant(tabIndex == 4))
-//				.onTapGesture { onButtonTapped(index: 4) }
-//			Spacer()
-//        }
-        
-        TabView {
-            ForEach(tabBarItems.indices, id: \.self) { index in
-                Text(tabBarItems[index])
-                    .onTapGesture {
-                        <#code#>
-                    }
-            }
+        HStack {
+            TabBarButton(text: "서머 바캉스", isSelected: .constant(tabIndex == 0))
+                .onTapGesture { onButtonTapped(index: 0) }
+            TabBarButton(text: "추천", isSelected: .constant(tabIndex == 1))
+                .onTapGesture { onButtonTapped(index: 1) }
+            TabBarButton(text: "랭킹", isSelected: .constant(tabIndex == 2))
+                .onTapGesture { onButtonTapped(index: 2) }
+            TabBarButton(text: "스타일", isSelected: .constant(tabIndex == 3))
+                .onTapGesture { onButtonTapped(index: 3) }
+            TabBarButton(text: "세일", isSelected: .constant(tabIndex == 4))
+                .onTapGesture { onButtonTapped(index: 4) }
+            Spacer()
         }
+    }
+    
+    private func onButtonTapped(index: Int) {
+        withAnimation { tabIndex = index }
+    }
+}
+
+struct TabBarButton: View {
+    let text: String
+    @Binding var isSelected: Bool
+    
+    var body: some View {
+        Text(text)
+            .fontWeight(isSelected ? .black : .regular)
+            .font(.system(size: 16))
+            .padding(10)
+            .overlay(
+                Rectangle()
+                    .frame(height: 4)
+                    .foregroundColor(isSelected ? .black : .clear),
+                alignment: .bottom
+            )
     }
 }
 
